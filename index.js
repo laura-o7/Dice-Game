d0757e381719231317300946065fc0c8d5c0240f
Laura <65500861+laura-o7@users.noreply.github.com>
2020-09-11 11:06:35 +0100

    Update files.
    Tidying up code.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImgSource1 = "images/dice" + randomNumber1 + ".png";
 
-var randomNumber2 = Math.floor(Math.random() * 6) +1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImgSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelector("img.img1").setAttribute("src",randomImgSource1)
+document.querySelector("img.img1").setAttribute("src", randomImgSource1)
 
 document.querySelector("img.img2").setAttribute("src", randomImgSource2)
 
 if (randomNumber1 > randomNumber2) {
-      document.querySelector("h1").innerHTML = "Player 1 wins! 🎉<br><span>Refresh me...</span>";
-    }
-    else if (randomNumber1 < randomNumber2) {
-      document.querySelector("h1").innerHTML = "Player 2 wins! 🎉<br><span>Refresh me...</span>";
-    }
-    else {
-      document.querySelector("h1").innerHTML = "Draw!<br><span>Refresh me...</span>";
-    }
+  document.querySelector("h1").innerHTML = "Player 1 wins! 🎉<br><span>Refresh me...</span>";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins! 🎉<br><span>Refresh me...</span>";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!<br><span>Refresh me...</span>";
+}
